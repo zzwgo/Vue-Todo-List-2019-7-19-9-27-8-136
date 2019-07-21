7b69b7e54ba55457db53a51cb7a98091bbb3ed84
zzw <kenzzw@163.com>
2019-07-21 20:47:32 +0800

    Add:  todolist demo

---
+++ demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <todoList/>
+  </div>
+</template>
+
+<script>
+import todoList from './components/todoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/src/components/todoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="todoList">
+    <span>Jquery To Do List</span>
+    <p>imple Todo List with adding and filter by diff status.</p>
+    <input v-model="item" placeholder=" " />
+    <button @click="addItem">Add</button>
+    <div>
+      <ol id="example" :class="{active:false}">
+        <li v-for="item in items" :key="item.id">
+          <input type="checkbox" @change="changeStatus(item.id)" />
+          {{ item.value }} {{ item.active }}
+        </li>
+      </ol>
+    </div>
+    <button>ALL</button>
+    <button>Active</button>
+    <button>Complete</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "todoList",
+  data: function() {
+    return {
+      item: "",
+      items: [],
+      nextTodoId: 0
+    };
+  },
+  methods: {
+    addItem() {
+      this.items.push({
+        id: this.nextTodoId++,
+        value: this.item,
+        active: true
+      }),
+        (this.item = "");
+    },
+    changeStatus(id) {
+      console.log(id);
+      this.items.find(it => it.id === id).active =
+        this.items.find(it => it.id === id).active == true ? false : true;
+    }
+  }
+};
+</script>
+
